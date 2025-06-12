1d43338367e8b74c46a938d98fcc3ad4b498a015
Lahari Mattapally <laharireddy883@gmail.com>
2025-06-12 16:23:56 -0400

    testing CI/CD feature

--- lambda/data_processor.py
+++ lambda/data_processor.py
@@ -90,4 +90,4 @@ def lambda_handler(event, context):
        return {
            'statusCode': 500,
            'body': json.dumps({'error': str(e)})
-       }# testing CI/CD feature "
+       }# testing CI/CD feature 1"
